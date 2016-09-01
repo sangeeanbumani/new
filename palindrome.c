d9c24e9422b6e00d2c59424f61e92df881094080
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-01 13:39:02 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x;
+while(x!=0)
+{
+n=n%10;
+rev=rev*10;
+n=n/10;
+}
+if(n==x)
+printf("palindrome");
+else 
+printf("not a palindrome");
+getch();
+}
